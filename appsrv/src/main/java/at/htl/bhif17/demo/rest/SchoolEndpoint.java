7d572e1a714efb88a0fe99a7bdfbda1e92f00551
Christian Aberger <c.aberger@htl-perg.ac.at>
2020-03-30 09:02:16 +0200

    add school REST

---
+++ appsrv/src/main/java/at/htl/bhif17/demo/rest/SchoolEndpoint.java
@@ -0,0 +1,28 @@
+package at.htl.bhif17.demo.rest;
+
+import at.htl.bhif17.demo.dao.SchoolDao;
+import at.htl.bhif17.demo.model.School;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("schools")
+@Produces(MediaType.APPLICATION_JSON)
+public class SchoolEndpoint {
+    @Inject SchoolDao schoolDao;
+
+    @GET
+    public List<School> all() {
+        return schoolDao.all();
+    }
+    @Path("/{id:[0-9]+}")
+    @GET
+    public School getSchool(@PathParam("id") int id) {
+        return schoolDao.findById(id);
+    }
+}
