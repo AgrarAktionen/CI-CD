73337c16d61e84da51c52875607bb4af236725f0
Christian Aberger <c.aberger@htl-perg.ac.at>
2020-05-15 10:08:33 +0200

    shadow dom, custom elements, lit-html

--- www/src/school.js
+++ www/src/school.js
@@ -1,14 +1,17 @@
+import {html, render} from 'https://unpkg.com/lit-html?module'
+
+let template = school => html`<div @click=${clicked}>Schule: ${school.name}</div>`
+
 class School extends HTMLElement {
     constructor() {
         super()
-        console.log("school constructor")
         this.shadow = this.attachShadow({mode: 'open'})
     }
     async connectedCallback() {
         let school = null
         try {
             school = await this.loadSchool()
-            this.fillElement()
+            render(template(school), this.shadow)
         } catch(e) {
             console.log("Exception gefangen", e)
             alert(`Fehler: ${e.message}`)
@@ -16,18 +19,14 @@ class School extends HTMLElement {
 
         console.log("school=", school)
     }
-    fillElement() {
-        let div = document.createElement("div")
-        div.innerHTML = "Hello, world!"
-        div.setAttribute("id", "mydiv")
-        this.shadow.append(div)
-    }
     async loadSchool() {
-        const url = "http://localhost:8080/schools"
+        const url = "http://localhost:8080/schools/1"
         const response = await fetch(url)
         const school = await response.json()
         return school
     }
 }
-
+function clicked(e) {
+    console.log("clicked...")
+}
 customElements.define("my-school", School)
