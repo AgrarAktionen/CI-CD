9fc5145ded78e03c78c09190594476f31e12aa8b
Christian Aberger <c.aberger@htl-perg.ac.at>
2020-05-18 11:27:51 +0200

    table demo

--- www/src/school.js
+++ www/src/school.js
@@ -1,29 +1,46 @@
 import {html, render} from 'https://unpkg.com/lit-html?module'
 
-let template = school => html`<div @click=${clicked}>Schule: ${school.name}</div>`
+let row = school => html`
+    <tr>
+        <td>${school.id}</td><td>${school.name}</td>
+    </tr>
+`
+let rows = schools => schools.map(school =>row(school))
 
+let template = schools => html`
+    <table>
+        <thead>
+        <tr>
+            <th>Id</th><th>Name</th>
+        </tr>
+        </thead>
+        <tbody>
+            ${rows(schools)}
+        </tbody>
+    </table>
+`
 class School extends HTMLElement {
     constructor() {
         super()
         this.shadow = this.attachShadow({mode: 'open'})
     }
     async connectedCallback() {
-        let school = null
+        let schools = null
         try {
-            school = await this.loadSchool()
-            render(template(school), this.shadow)
+            schools = await this.loadSchools()
+            render(template(schools), this.shadow)
         } catch(e) {
             console.log("Exception gefangen", e)
             alert(`Fehler: ${e.message}`)
         }
 
-        console.log("school=", school)
+        console.log("schools=", schools)
     }
-    async loadSchool() {
-        const url = "http://localhost:8080/schools/1"
+    async loadSchools() {
+        const url = "http://localhost:8080/schools"
         const response = await fetch(url)
-        const school = await response.json()
-        return school
+        const schools = await response.json()
+        return schools
     }
 }
 function clicked(e) {
