64a9dd1b8a8a67735ace2169e433f5ec1dd9eba5
caberger <c.aberger@htl-perg.ac.at>
2020-09-26 12:57:26 +0200

    add webpack

---
+++ www/src/app/app.js
@@ -0,0 +1,22 @@
+import {render, html} from "lit-html"
+import "./school"
+
+class Application extends HTMLElement {
+    constructor() {
+        super()
+        this.attachShadow({ mode: "open" })
+    }
+    connectedCallback() {
+        this.render()
+    }
+    template() {
+        return html`
+            <h1>My First Application</h1>
+            <my-school></my-school>
+        `
+    }
+    render() {
+        render(this.template(), this.shadowRoot)
+    }
+}
+customElements.define("my-app", Application)
