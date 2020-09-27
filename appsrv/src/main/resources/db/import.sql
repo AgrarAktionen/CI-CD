f50383b207622fb44e25105b5b3a090217954da0
caberger <c.aberger@htl-perg.ac.at>
2020-09-27 09:14:37 +0200

    quarkus sql drop-and-create

---
+++ appsrv/src/main/resources/db/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO School(name, street) values('HTL-Perg', 'Machlandstr. 48');
+INSERT INTO School(name, street) values('HTL-Leonding', 'Limesstrasse 8-12');
+
+INSERT INTO db.Person(LastName, FirstName, MatNr, school_id) VALUES('Doe', 'John', '1234', 1);
+INSERT INTO db.Person(LastName, FirstName, MatNr, school_id) VALUES('Roe', 'Jane', '5678', 1);
+INSERT INTO db.Person(LastName, FirstName, MatNr, school_id) VALUES('Sixpack', 'Joe', '9012', 1);
+
