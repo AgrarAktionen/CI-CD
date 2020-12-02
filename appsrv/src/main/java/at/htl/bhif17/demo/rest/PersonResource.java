97f5e70bf9ba4b7dc4d14974b2be42115018ecb2
caberger <c.aberger@htl-perg.ac.at>
2020-12-02 08:53:02 +0100

    file download

--- appsrv/src/main/java/at/htl/bhif17/demo/rest/PersonResource.java
+++ appsrv/src/main/java/at/htl/bhif17/demo/rest/PersonResource.java
@@ -2,12 +2,16 @@ package at.htl.bhif17.demo.rest;
 
 import at.htl.bhif17.demo.dao.PersonDao;
 import at.htl.bhif17.demo.model.Person;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.*;
 import java.util.List;
 
 @Path("person")
@@ -40,5 +44,19 @@ public class PersonResource {
         var person = personDao.get(id);
         personDao.remove(person);
         return Response.status(Response.Status.NO_CONTENT).build();
+    }
+    @GET
+    @Path("/download")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response downloadAllPersons() {
+        System.out.println("send persons...");
+
+        var home = System.getProperty("user.home");
+        var homeDirectory = new File(home);
+        var deskTop = new File(homeDirectory, "Desktop");
+        var testFile = new File(deskTop, "test.txt");
+        var persons = personDao.getAll();
+        //TODO: write persons to a testFile as a .csv file and send it as a response.
+        return Response.ok(testFile).build();
     }
 }
