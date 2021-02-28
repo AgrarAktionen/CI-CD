e970dc29bd0387b0daa52873553aa2375163ac2f
qrzbrz <qrzbrz@yahoo.de>
2021-02-28 12:22:09 +0100

    add nginx

--- docker/appsrv/scripts/run.sh
+++ docker/appsrv/scripts/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd /deployments
-java -Dquarkus.profile=docker -jar appsrv-0.5.0-runner.jar
+RUNNER=$(ls application-server-*-runner.jar)
+
+echo "starting quarkus $RUNNER..."
+java -Dquarkus.profile=docker -jar $RUNNER
