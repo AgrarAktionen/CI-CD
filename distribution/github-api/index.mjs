fcea42e455e65ab6724fcb256f6c8acdcd281ac1
qrzbrz <qrzbrz@yahoo.de>
2021-03-01 22:26:28 +0100

    staging

--- distribution/github-api/index.mjs
+++ distribution/github-api/index.mjs
@@ -7,7 +7,6 @@ const token = process.argv[3]
 const basicAuth = Buffer.from(`${user}:${token}`).toString("base64")
 
 const authHeader = `Basic ${basicAuth}`
-console.log(`user=${user}, token=${token}, auth=${authHeader}`)
 const url = `${base}/repos/caberger/javafx-cdi-jpa/releases`
 
 async function releases() {
@@ -18,7 +17,8 @@ async function releases() {
         }
     })
     const releases = response.data
-    releases.forEach(r => console.log("-->", r))
+    releases.map(r => r.tag_name).forEach(n => console.log("tag", n))
+    //releases.forEach(r => console.log("-->", r))
 }
 releases()
 
