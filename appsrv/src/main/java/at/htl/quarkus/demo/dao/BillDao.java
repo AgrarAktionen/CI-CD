dac4957a6fe7c6b6038fda623d4561692c768295
20170032 <20170032@students.htl-perg.ac.at>
2021-03-15 09:32:31 +0100

    add composite key example

---
+++ appsrv/src/main/java/at/htl/quarkus/demo/dao/BillDao.java
@@ -0,0 +1,29 @@
+package at.htl.quarkus.demo.dao;
+
+import at.htl.quarkus.demo.model.Bill;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Named
+@Dependent
+public class BillDao {
+    @Inject
+    EntityManager em;
+
+    public List<Bill> getAll() {
+        return em.createQuery("select b from Bill b", Bill.class).getResultList();
+    }
+    public Bill get(int id) {
+        return em.find(Bill.class, id);
+    }
+    public Bill save(Bill bill) {
+        return em.merge(bill);
+    }
+    public void delete(Bill bill) {
+        em.remove(bill);
+    }
+}
