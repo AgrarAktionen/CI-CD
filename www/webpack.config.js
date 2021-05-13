b504919ebdba7589145c5242efe80c6d41f38045
Christian Aberger <c.aberger@htl-perg.ac.at>
2021-05-13 09:42:00 +0200

    add webjar

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -14,7 +14,8 @@ const options = env => {
     return {
         isDebug,
         ENV: isDebug ? 'development' : 'production',
-        OUTPUT_PATH: isDebug ? resolve(__dirname, ".") : resolve(__dirname, '../appsrv/src/main/resources/META-INF/resources/'),
+        //OUTPUT_PATH: isDebug ? resolve(__dirname, ".") : resolve(__dirname, '../appsrv/src/main/resources/META-INF/resources/'),
+        OUTPUT_PATH: isDebug ? resolve(__dirname, ".") : resolve(__dirname, `target/classes/META-INF/resources/webjars/www/${packageJson.version}`),
         publicPath: isDebug ? "auto" : "/"
     }
 }
