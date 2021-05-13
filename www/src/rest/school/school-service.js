a46630898fa30fd1667680ee66bf5decafe7809b
Christian Aberger <c.aberger@htl-perg.ac.at>
2021-05-13 16:55:29 +0200

    rewrite

--- www/src/rest/school/school-service.js
+++ www/src/rest/school/school-service.js
@@ -1,7 +1,7 @@
 import {setSchools} from "Model/school/school-action-creator"
 
 export async function loadSchools() {
-    const response = await fetch("/school")
+    const response = await fetch("./school")
     const schools = await response.json()
     setSchools(schools)
 }
