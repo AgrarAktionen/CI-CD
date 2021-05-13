00e7586e1cb905e39bbb0d645b9656eaa72821d0
Christian Aberger <c.aberger@htl-perg.ac.at>
2021-05-13 12:51:42 +0200

    deploy to cloud - 1

--- k8s/create-secret.sh
+++ k8s/create-secret.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-NAMESPACE=default
-kubectl delete -n $NAMESPACE secret leocloudcredentials
+kubectl delete secret leocloudcredentials
 kubectl create secret generic leocloudcredentials \
-    --namespace=$NAMESPACE \
     --from-file=.dockerconfigjson=$HOME/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson
