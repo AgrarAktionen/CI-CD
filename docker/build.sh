a9bd356986af3e4afe5331b777810dd5b555997f
qrzbrz@yahoo.de <qrzbrz@yahoo.de>
2021-06-25 10:31:44 +0200

    push nginx

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-USERNAME=$1
-
+USERNAME=$2
+IMAGES=appsrv express nginx
 pushd $SCRIPT_DIR
-docker build appsrv --tag registry.cloud.htl-leonding.ac.at/${USERNAME}/appsrv || exit 1
-docker build express --tag registry.cloud.htl-leonding.ac.at/${USERNAME}/express || exit 2
-docker build nginx --tag registry.cloud.htl-leonding.ac.at/${USERNAME}/nginx || exit 3
+
+if [[ "$1." == "build."]]
+then
+    for image in $IMAGES
+    do
+        docker build $image --tag registry.cloud.htl-leonding.ac.at/${USERNAME}/$image || exit 3
+    done
+elif [[ "$1." == "push."]]
+then
+    for image in $IMAGES
+    do
+        docker push registry.cloud.htl-leonding.ac.at/${USERNAME}/$image || exit 2
+    done
+else
+    echo "usage: $0 build|push <username>"
+    exit 1
+fi
+
 popd
