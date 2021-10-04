e2c5fb08ce28d80fbe6cbedad501fccbcb6c95d0
maximilian-grabmann <56428854+maximilian-grabmann@users.noreply.github.com>
2021-10-04 16:33:14 +0200

    Update ItemInserter.java

--- appsrv/src/main/java/com/aktionen/agrar/download/ItemInserter.java
+++ appsrv/src/main/java/com/aktionen/agrar/download/ItemInserter.java
@@ -6,11 +6,12 @@ import com.aktionen.agrar.model.Item;
 import com.aktionen.agrar.model.Price;
 import io.quarkus.scheduler.Scheduled;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.inject.Named;
+import javax.transaction.SystemException;
 import javax.transaction.Transactional;
-import java.io.FileNotFoundException;
+import javax.transaction.UserTransaction;
 import java.io.IOException;
 import java.util.List;
 
@@ -27,7 +28,7 @@ public class ItemInserter {
     @Inject
     PriceDao priceDao;
 
-
+/*
     @Scheduled(every="60m")
     public void insertData() throws IOException {
         csvDownloader.fetchCSV();
@@ -40,6 +41,26 @@ public class ItemInserter {
 
         List<Price> prices = csvDownloader.createPriceList();
         priceDao.insertAll(prices);
+    }
+*/
+    @Inject
+    UserTransaction userTransaction;
+
+    @PostConstruct
+    public void init() throws SystemException {
+        //set a timeout as high as you need
+        userTransaction.setTransactionTimeout(7200);
+    }
+
+    @Transactional
+    @Scheduled(every = "100s")
+    public void process() throws IOException {
+        csvDownloader.fetchCSV();
+        List<Item> items = csvDownloader.createItemList();
+        List<Price> prices = csvDownloader.createPriceList();
+        itemDao.insertAll(items, "Faie");
+        priceDao.insertAll(prices);
+
     }
 
 }
