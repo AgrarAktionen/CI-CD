d53ef11eb7af035bbfa2c1092f311cdfad5c4534
MaximilianGrabmann <56428854+maximilian-grabmann@users.noreply.github.com>
2021-10-12 09:19:29 +0200

    Rebuild

--- appsrv/src/main/resources/db/import.sql
+++ appsrv/src/main/resources/db/import.sql
@@ -1,13 +1,5 @@
 UPDATE hibernate_sequence set next_val = 1000;
 
-insert into db.Shop(shopId, name, contactPerson) values (1, 'Faie', 'Faie-Manager');
-insert into db.Shop(shopId, name, contactPerson) values (2, 'Test1', 'Faie-Manager');
-insert into db.Shop(shopId, name, contactPerson) values (3, 'Test2', 'Faie-Manager');
-
-insert into db.APILink(apiId, url, description, user, password, shopId) values (1, 'https://www.faie.at/backend/export/index/agraraktionen.csv?feedID=68&hash=1bfdc5718d84ebfd191e9ee6617a7764', 'Faie', '', '', 1);
-insert into db.APILink(apiId, url, description, user, password, shopId) values (2, '', '', 'wdawd', 'awd', 2);
-insert into db.APILink(apiId, url, description, user, password, shopId) values (3, '', '', 'awd', 'awd', 2);
-
 INSERT INTO School(id, name, street) values
     (1, 'HTL-Leonding', 'Limesstrasse 8-12'),
     (2, 'HTL-Perg', 'Machlandstr. 48');
