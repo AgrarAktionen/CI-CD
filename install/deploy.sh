2757e58979aa5c4ec93dbb7c8694c7677c60b9ed
maximilian-grabmann <56428854+maximilian-grabmann@users.noreply.github.com>
2021-10-13 21:39:28 +0200

    Update deploy.sh

--- install/deploy.sh
+++ install/deploy.sh
@@ -5,7 +5,7 @@ DEPLOY_FOLDER=/usr/share/appsrv
 rm -rf $DEPLOY_FOLDER
 mkdir -p $DEPLOY_FOLDER
 
-cp -r appsrv/target/{application-server-*-runner.jar, lib/} $DEPLOY_FOLDER
+cp -r appsrv/target/{application-server-*-runner.jar.original, lib/} $DEPLOY_FOLDER
 chown -R quarkus:quarkus $DEPLOY_FOLDER
 
 
