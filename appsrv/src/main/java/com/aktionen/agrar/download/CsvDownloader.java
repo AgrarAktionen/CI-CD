123a0375adb31b616ae068b4d39d6346fd2d47c2
maximilian-grabmann <56428854+maximilian-grabmann@users.noreply.github.com>
2021-10-15 10:49:37 +0200

    Update CsvDownloader.java

--- appsrv/src/main/java/com/aktionen/agrar/download/CsvDownloader.java
+++ appsrv/src/main/java/com/aktionen/agrar/download/CsvDownloader.java
@@ -23,7 +23,7 @@ import java.util.List;
 @QuarkusMain
 public class CsvDownloader {
 
-    String fileName = "https://student.cloud.htl-leonding.ac.at/20170011/var/lib/appsrv/file.csv";
+    String fileName = "var/lib/appsrvstorage/file.csv";
     List<Item> items = createItemList();
     List<Price> prices = createPriceList();
 
@@ -62,7 +62,7 @@ public class CsvDownloader {
 
     public static void fetchCSV() throws IOException {
         InputStream inputStream = new URL("https://www.faie.at/backend/export/index/agraraktionen.csv?feedID=68&hash=1bfdc5718d84ebfd191e9ee6617a7764").openStream();
-        FileOutputStream fileOS = new FileOutputStream("https://student.cloud.htl-leonding.ac.at/20170011/var/lib/appsrv/file.csv");
+        FileOutputStream fileOS = new FileOutputStream("var/lib/appsrvstorage/file.csv");
         int i = IOUtils.copy(inputStream, fileOS);
 
     }
