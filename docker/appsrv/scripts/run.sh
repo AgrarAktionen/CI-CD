626b33f79e5841df27c2e3a28dbe66a8f683d8a7
XaverMitterlehner <43880368+XaverMitterlehner@users.noreply.github.com>
2021-10-21 16:24:57 +0200

    Update run.sh

--- docker/appsrv/scripts/run.sh
+++ docker/appsrv/scripts/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-RUNNER=$(ls application-server-*-runner.jar)
+RUNNER=$(ls *-runner.jar)
+
+while ! nc -z mysql 3306; do   
+    echo "waiting for godot..."
+    sleep 1
+done
 
 echo "starting quarkus $RUNNER..."
 java -Dquarkus.profile=prod -jar $RUNNER
