814ef6e4e90ee359a978e7c0cc7b5f33054c2c37
maximilian-grabmann <56428854+maximilian-grabmann@users.noreply.github.com>
2021-12-09 11:03:19 +0100

    Update CsvDownloader.java

--- appsrv/src/main/java/com/aktionen/agrar/download/CsvDownloader.java
+++ appsrv/src/main/java/com/aktionen/agrar/download/CsvDownloader.java
@@ -171,7 +171,7 @@ public class CsvDownloader {
 
 
 
-    @Scheduled(every = "4s", delayed = "100s") //120s
+    @Scheduled(every = "4s", delayed = "120s") //120s
     @Transactional
     public void process() throws IOException, TranslateException, ImageReadException, ModelException {
 
@@ -189,7 +189,7 @@ public class CsvDownloader {
 
     }
 
-    @Scheduled(every = "1000s") //100s are only for test purpose, it should be reloaded once a day in reality
+    @Scheduled(every = "1500s") //100s are only for test purpose, it should be reloaded once a day in reality
     @Transactional
     public void csv() throws IOException, NoSuchAlgorithmException {
         System.out.println("Downloading CSV...");
@@ -201,7 +201,7 @@ public class CsvDownloader {
         //toCSV(toImport);
     }
 
-    @Scheduled(every = "1s", delayed = "100s")
+    @Scheduled(every = "3s", delayed = "120s")
     @Transactional
     public void cleaner() throws IOException, NoSuchAlgorithmException {
         CheckSum currentCheckSum = fetchCSV();
