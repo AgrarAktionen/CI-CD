ba882b70a48b9e0b9304e5005bca3b513eb51f80
MaximilianGrabmann <56428854+maximilian-grabmann@users.noreply.github.com>
2021-12-13 13:58:42 +0100

    update version

--- docker/build.sh
+++ docker/build.sh
@@ -3,19 +3,20 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 USERNAME=$2
 IMAGES="appsrv nginx"
+VERSION="0.1.0"
 pushd $SCRIPT_DIR
 
 if [[ "$1." == "build." ]]
 then
     for image in $IMAGES
     do
-        docker build $image --tag registry.cloud.htl-leonding.ac.at/${USERNAME}/$image || exit 3
+        docker build $image --tag registry.cloud.htl-leonding.ac.at/${USERNAME}/$image:$VERSION || exit 3
     done
 elif [[ "$1." == "push." ]]
 then
     for image in $IMAGES
     do
-        docker push registry.cloud.htl-leonding.ac.at/${USERNAME}/$image || exit 2
+        docker push registry.cloud.htl-leonding.ac.at/${USERNAME}/$image:$VERSION || exit 2
     done
 else
     echo "usage: $0 build|push <username>"
